2fd2982a01df1eb16cf817ee0889f194ff1dd7d3
Angus Salkeld <angus.salkeld@nitric.io>
2022-09-21 13:21:04 +1000

    Add the function

--- go.mod
+++ go.mod
@@ -1,3 +1,25 @@
 module github.com/nitrictech/gcp-alert-to-discord-webhook
 
 go 1.18
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/nitrictech/go-sdk v0.9.0-rc.25
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/envoyproxy/protoc-gen-validate v0.6.7 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/missionMeteora/toolkit v0.0.0-20170713173850-88364e3ef8cc // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/nitrictech/apis v0.17.0 // indirect
+	github.com/nitrictech/protoutils v0.0.0-20220321044654-02667a814cdf // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3 // indirect
+	google.golang.org/grpc v1.46.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,99 @@
 package main
 
-func main(){
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/sirupsen/logrus"
+
+	"github.com/nitrictech/go-sdk/faas"
+	"github.com/nitrictech/go-sdk/resources"
+)
+
+type RequiredEnv struct {
+	ProductionDiscordWebhook string `envconfig:"PRODUCTION_DISCORD_WEBHOOK" required:"true"`
+	StagingDiscordWebhook    string `envconfig:"STAGING_DISCORD_WEBHOOK" required:"true"`
+}
+
+var Environment RequiredEnv
+
+type webhook struct {
+	discordWebhook string
+}
+
+func (w *webhook) handleWebhook(hc *faas.HttpContext, hh faas.HttpHandler) (*faas.HttpContext, error) {
+	md := map[string]any{}
+	rd := map[string]any{
+		"content": "",
+	}
+
+	err := json.Unmarshal(hc.Request.Data(), &md)
+	if err != nil {
+		// assume not json
+		rd["content"] = string(hc.Request.Data())
+	} else {
+		rd["content"] = md
+	}
+
+	b, err := json.Marshal(rd)
+	if err != nil {
+		logrus.Info(err)
+		hc.Response.Status = http.StatusBadRequest
+		return hh(hc)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, w.discordWebhook, bytes.NewReader(b))
+	if err != nil {
+		logrus.Info(err)
+		hc.Response.Status = http.StatusBadRequest
+		return hh(hc)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	cli := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	_, err = cli.Do(req)
+	if err != nil {
+		logrus.Info(err)
+		hc.Response.Status = http.StatusInternalServerError
+		return hh(hc)
+	}
+
+	return hh(hc)
+}
+
+func run() error {
+	err := envconfig.Process("", &Environment)
+	if err != nil {
+		return err
+	}
+
+	mainAPI, err := resources.NewApi("webhook")
+	if err != nil {
+		return err
+	}
+
+	sw := &webhook{discordWebhook: Environment.StagingDiscordWebhook}
+	mainAPI.Post("/staging", sw.handleWebhook)
+
+	pw := &webhook{discordWebhook: Environment.ProductionDiscordWebhook}
+	mainAPI.Post("/production", pw.handleWebhook)
+
+	return resources.Run()
+}
+
+func main() {
+	if err := run(); err != nil {
+		if strings.Contains(err.Error(), "EOF") {
+			logrus.Info("Shutting down")
+		} else {
+			panic(err)
+		}
+	}
 }
